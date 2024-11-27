f149cf397c7c9b3a40d213260cde9bda361fdfbe
Louise Anderson <ddq4yosl@students.codeinstitute.net>
2024-11-27 14:37:59 +0000

    create task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+# Task model 
+
+class Task (models.Model):
+
+    options = (
+        ("pending", "Pending"),
+        ("in_progress", "In Progress"),
+        ("completed", "Completed")
+    )
+    
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    status = models.CharField(max_length=15, choices=options, default='pending')
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
